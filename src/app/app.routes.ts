663b18e384bce10d347c5b5d15eb9f84f31b60ab
Asrar Memon <asrar.memon0001@gmail.com>
2024-05-26 20:39:59 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { AuthComponent } from './auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "users",
+        component: UsersComponent
+    },
+    {
+        path: "",
+        component: AuthComponent
+    }
+];
